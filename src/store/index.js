fbe373ed3927db9356592064e57b964b3f5e37cd
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-10-09 23:43:31 +0200

    :bug: Load sagas

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,17 @@
 //@flow
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import sagas from './sagas';
 import reducers from './reducers';
 
 const DEFAULT_STATE = {};
 
-const enhancer = compose(applyMiddleware(createSagaMiddleware()));
+const sagaMiddleware = createSagaMiddleware();
 
-export default createStore(reducers, DEFAULT_STATE, enhancer);
+const enhancer = compose(applyMiddleware(sagaMiddleware));
+
+const store = createStore(reducers, DEFAULT_STATE, enhancer);
+
+sagaMiddleware.run(sagas);
+
+export default store;
