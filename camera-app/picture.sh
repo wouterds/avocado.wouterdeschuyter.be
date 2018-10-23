c4b9583cc38d64c4847761719646498c69e1ed3e
Raspberry Pi 3 <pi@raspberry>
2018-10-23 09:03:58 +0000

    :bug: $PATH is a special variable, whoops

--- camera-app/picture.sh
+++ camera-app/picture.sh
@@ -5,13 +5,13 @@ cd "$(dirname "$0")"
 
 # Dynamic file name
 FILE=`date '+%Y%m%d%H%M'`.jpg
-PATH=./images/$FILE
+FILE_PATH=./images/$FILE
 
 # Take picture
-fswebcam -c ./webcam.conf $PATH
+fswebcam -c ./webcam.conf $FILE_PATH
 
 # Display on TFT screen
-sudo fbi -T 2 -d /dev/fb1 -noverbose -a $PATH
+sudo fbi -T 2 -d /dev/fb1 -noverbose -a $FILE_PATH
 
 # Copy to webserver pi
 rsync --ignore-existing --recursive ./images/* pi@server01.wouterdeschuyter.be:~/docker/sites/be.wouterdeschuyter.avocado/images
