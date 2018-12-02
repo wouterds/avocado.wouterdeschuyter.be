b3b560d6d0d43aced27de74a5a776f0e035fa766
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-12-02 14:50:26 +0100

    :sparkles: Generate all-time video

---
+++ src/server/scripts/generate-all-time.js
@@ -0,0 +1,14 @@
+#!./node_modules/.bin/babel-node
+//@flow
+import path from 'path';
+import { generate as generateVideo } from 'server/helpers/video';
+
+/*global __dirname: true*/
+
+generateVideo(
+  path.resolve(__dirname, '../../../images'),
+  path.resolve(__dirname, '../../../videos'),
+  'all-time',
+  new Date(0),
+  new Date(),
+);
